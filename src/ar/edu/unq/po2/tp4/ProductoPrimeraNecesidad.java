d3e0ab5fe4669cc0bcd991e0fd880f0a4641f851
Agustin Lascar <aguslascar@hotmail.com.ar>
2022-04-17 19:04:11 -0300

    Ejercicio 2 - Primera necesidad

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -1,18 +1,30 @@
 package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto{
+	
+	private double descuento = 0.0;
 
-	public ProductoPrimeraNecesidad(String nombre, double precio) {
+	private double getDescuento() {
+		return descuento;
+	}
+
+	private void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+
+	public ProductoPrimeraNecesidad(String nombre, double precio, double descuento) {
 		super(nombre, precio);
+		this.setDescuento(descuento);
 	}
 	
-	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esCuidado) {
+	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esCuidado, double descuento) {
 		super(nombre, precio, esCuidado);
+		this.setDescuento(descuento);
 	}
 	
 	@Override
 	public double getPrecio() {
-		return super.getPrecio() * 0.9;
+		return super.getPrecio() * this.getDescuento();
 		
 	}
 	
--- test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
+++ test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
@@ -6,16 +6,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductoPrimeraNecesidadTest {
+	
+	/**
+	 * Test modificado para incluir descuentos variables en productos.
+	 */
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@BeforeEach
 	public void setUp() {
-		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		leche = new ProductoPrimeraNecesidad("Leche", 8d, false, 0.89);
+		arroz = new ProductoPrimeraNecesidad("Arroz", 8d, false, 0.92);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
-		assertEquals(7.2, leche.getPrecio());
+		assertEquals(7.12, leche.getPrecio());
+		assertEquals(7.36, arroz.getPrecio());
 	}
 }
